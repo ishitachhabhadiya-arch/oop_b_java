da35cf96b198a888ef83b041624febe53b9d325a
kelvinokumu <oumakelvinokumu@gmail.com>
2026-01-20 10:31:11 +0300

    some changes

--- src/week1_2_intro/App.java
+++ src/week1_2_intro/App.java
@@ -1,7 +1,26 @@
 package week1_2_intro;
 
 public class App {
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        
+        display();
+    }
+
+    public static void display(){
+        int a = 123;
+        float b = 123.12F;
+        double c = 12.456;
+        String name = "OOP 1";
+        boolean flag = true;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(name);
+        System.out.println(flag);
+
+
     }
 }
